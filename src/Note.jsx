5b2cd8ffa142b3cc62a5201c7c5ad4e24add8fa5
pramod kumar <41004828+only-pramod@users.noreply.github.com>
2021-01-04 13:58:44 +0530

    Initial commit

---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> heading </h1>
+      <p> paragraph </p>
+    </div>
+  );
+}
+
+export default Note;
